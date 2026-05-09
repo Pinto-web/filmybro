1a0001a0b7efdbe3a877dfbef8b8082d6b817260
Pinto-web <pintonsebastian1@gmail.com>
2026-05-09 11:34:46 +0530

    Configured backend for vercel

--- backend/server.js
+++ backend/server.js
@@ -42,4 +42,8 @@ const connectDB = async () => {
 };
 connectDB();
 
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+if (process.env.NODE_ENV !== 'production') {
+  app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+}
+
+module.exports = app;
